e956b4b032f28d11b6564915aab4842f496448c7
fishermanswharff <fishermanswharff@github.com>
2015-01-07 01:05:37 -0500

    Autopush GradingWizard App Master Branch

--- app/controllers/repos_controller.rb
+++ app/controllers/repos_controller.rb
@@ -4,6 +4,7 @@ class ReposController < ApplicationController
   def index
     Rails.cache.fetch([:ga_repos,self], expires_in: 5.minutes) do
       @repos = RepoGetter::Get::get_repos('https://api.github.com/orgs/ga-wdi-boston/repos?per_page=100')
+      links_string = @repos.header.fetch('link') #=> "<https://api.github.com/organizations/6361298/repos?per_page=100&page=2>; rel=\"next\", <https://api.github.com/organizations/6361298/repos?per_page=100&page=4>; rel=\"last\""
       @repos = JSON.parse(@repos.body)
     end
   end
