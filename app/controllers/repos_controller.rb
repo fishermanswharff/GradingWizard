b0617960d0cd90c0fcb3f9c6d12e8fabad430539
fishermanswharff <fishermanswharff@github.com>
2015-01-08 13:02:52 -0500

    Autopush GradingWizard App Master Branch

--- app/controllers/repos_controller.rb
+++ app/controllers/repos_controller.rb
@@ -2,12 +2,8 @@ require Rails.root.join('lib/modules/RepoGetter')
 class ReposController < ApplicationController
 
   def index
-    Rails.cache.fetch([:ga_repos,self], expires_in: 5.minutes) do
-      client = Octokit::Client.new(:access_token => ENV['GITHUB_TOKEN'])
-      @repos = client.repos('ga-wdi-boston', :per_page => 100)
-      last_response = client.last_response
-      number_of_pages = last_response.rels[:last].href.match(/page=(\d+)$/)[1]
-    end
+    binding.pry
+    @repos = Repo.get_repos
   end
 
   def create
--- app/models/repo.rb
+++ app/models/repo.rb
@@ -1,3 +1,12 @@
 class Repo < ActiveRecord::Base
   validates :name, :url, presence: true
+
+  def self.get_repos
+    Rails.cache.fetch([:ga_repos,self], expires_in: 5.minutes) do
+      client = Octokit::Client.new(:access_token => ENV['GITHUB_TOKEN'])
+      repos = client.repos('ga-wdi-boston', :per_page => 100)
+      # last_response = client.last_response
+      # number_of_pages = last_response.rels[:last].href.match(/page=(\d+)$/)[1]
+    end
+  end
 end
